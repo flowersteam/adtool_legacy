8e7d8805fc3debb4b0861aaf3f0d04a2feaa9775
ClementRomac <clement.romac@gmail.com>
2021-02-23 18:19:19 +0100

    Starting AutoDisc architecture + Lenia's static output representation

---
+++ Libs/AutoDisc/OutputWrappers/AbstractOutputWrapper.py
@@ -0,0 +1,36 @@
+from Libs.Utils.AttrDict import AttrDict
+
+class AbstractOutputWrapper ():
+    """ Base class to map the observations of a system to an embedding vector (BC characterization)
+    """
+
+    CONFIG_DEFINITION = []
+
+    @classmethod
+    def get_default_config(cls):
+        default_config = AttrDict()
+        for param in cls.CONFIG_DEFINITION:
+            param_dict = param.to_dict()
+            default_config[param_dict['name']] = param_dict['default']
+        
+        return default_config
+
+    def __init__(self, **kwargs):
+        self.config = self.get_default_config()
+        self.config.update(kwargs)
+
+    def calc(self, observations, **kwargs):
+        """ Maps the observations of a system to an embedding
+            #TODO: space of possible embeddings as in https://github.com/openai/gym/tree/master/gym/spaces
+            #TODO: allow to calc batch of observations
+            Args:
+                observations (AttrDict):
+            Returns
+                embeddings (AttrDict): generally vector but we might need AttrDict structures, for instance for IMGEP-HOLMES
+        """
+        raise NotImplementedError
+
+    def calc_distance(self, embedding_a, embedding_b, **kwargs):
+        """ Compute the distance between 2 embedding
+        """
+        raise NotImplementedError
---
+++ Libs/AutoDisc/OutputWrappers/LeniaOutputRepresentation.py
@@ -0,0 +1,69 @@
+from Libs.AutoDisc.OutputWrappers.AbstractOutputWrapper import AbstractOutputWrapper
+from Libs.Utils.AutoDiscParameter import AutoDiscParameter, ParameterTypesEnum
+from Libs.Utils.torch_utils import roll_n
+import torch
+import numpy as np
+
+class LeniaImageRepresentation(AbstractOutputWrapper):
+    CONFIG_DEFINITION = [
+        AutoDiscParameter(
+                    name="distance_function", 
+                    type=ParameterTypesEnum.get('STRING'), 
+                    values_range=["L2"], 
+                    default="L2"),
+        AutoDiscParameter(
+                    name="env_size", 
+                    type=ParameterTypesEnum.get('ARRAY', dims=[2]), 
+                    values_range=[1, np.inf], 
+                    default=[256, 256])
+    ]
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.n_latents = self.config.env_size[0] * self.config.env_size[1]
+
+
+    def calc(self, observations):
+        """
+            Maps the observations of a system to an embedding vector
+            Return a torch tensor
+        """
+        # filter low values
+        filtered_im = torch.where(observations.states[-1] > 0.2, observations.states[-1], torch.zeros_like(observations.states[-1]))
+
+        # recenter
+        mu_0 = filtered_im.sum()
+        if mu_0.item() > 0:
+
+            # implementation of meshgrid in torch
+            x = torch.arange(self.config.env_size[0])
+            y = torch.arange(self.config.env_size[1])
+            xx = x.repeat(self.config.env_size[1], 1)
+            yy = y.view(-1, 1).repeat(1, self.config.env_size[0])
+            X = (xx - int(self.config.env_size[0] / 2)).float()
+            Y = (yy - int(self.config.env_size[1] / 2)).float()
+
+            centroid_x = ((X * filtered_im).sum() / mu_0).round().int().item()
+            centroid_y = ((Y * filtered_im).sum() / mu_0).round().int().item()
+
+            filtered_im = roll_n(filtered_im, 0, centroid_x)
+            filtered_im = roll_n(filtered_im, 1, centroid_y)
+
+        embedding = filtered_im.flatten()
+
+        return embedding
+
+
+    def calc_distance(self, embedding_a, embedding_b):
+        """
+            Compute the distance between 2 embeddings in the latent space
+            /!\ batch mode embedding_a and embedding_b can be N*M or M
+        """
+        # l2 loss
+        if self.config.distance_function == "L2":
+            dist = (embedding_a - embedding_b).pow(2).sum(-1).sqrt() - 0.5 * embedding_b.pow(2).sum(-1).sqrt() # add regularizer to avoid dead outcomes
+
+        else:
+            raise NotImplementedError
+
+        return dist
