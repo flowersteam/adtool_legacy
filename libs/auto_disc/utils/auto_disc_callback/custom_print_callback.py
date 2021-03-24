ae698ff945e6e185302bf5bfcdb302bd3c5e6fab
PERIE Mathieu <mathieu.perie@inria.fr>
2021-03-24 11:14:46 +0100

    #38 cleaning and adding commentary

--- libs/auto_disc/utils/auto_disc_callback/custom_print_callback.py
+++ libs/auto_disc/utils/auto_disc_callback/custom_print_callback.py
@@ -2,8 +2,15 @@ from libs.auto_disc.utils import BaseAutoDiscCallback
 
 class CustomPrintCallback(BaseAutoDiscCallback):
     def __init__(self, custom_message_to_print):
+        """
+        init the callback with a message to print
+        custom_message_to_print: string
+        """
         super().__init__()
         self._custom_message_to_print = custom_message_to_print
 
     def __call__(self, **kwargs):
+        """
+        callback print a message
+        """
         print(self._custom_message_to_print)
--- libs/auto_disc/utils/auto_disc_callback/custom_save_callback.py
+++ libs/auto_disc/utils/auto_disc_callback/custom_save_callback.py
@@ -5,16 +5,20 @@ import matplotlib.pyplot as plt
 
 class CustomSaveCallback(BaseAutoDiscCallback):
     def __init__(self, folder_path):
+        """
+        init the callback with a path to a folder to save discoveries
+        folder_path: string, path to folder
+        """
         super().__init__()
         self.folder_path = folder_path
-        #self._output_representations = output_representations
 
     def __call__(self, pipeline, run_idx, raw_run_parameters, run_parameters, raw_output, output, rendered_output, step_observations):
-        # save_folder="/home/mperie/project/save_callback/"
+        """
+        callback saves the 'rendered_output' (the discoveries) as a jpg and the input parameters 'run_parameters' in a pickle in folder
+        define by 'self.folder_path'
+        """
         plt.imsave(self.folder_path+"output/"+str(run_idx)+".jpg", rendered_output, format='jpg')
         pickle_file=self.folder_path+"run_parameters/"+str(run_idx)
-        with open(pickle_file, 'wb') as f1:
-            pickle.dump(rendered_output, f1)
 
         with open(pickle_file, 'wb') as f1:
             pickle.dump(run_parameters, f1)
