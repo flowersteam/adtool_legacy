c90bb7f3d0d0aa9a253769c8b2bcb17155dc8a99
ClementRomac <clement.romac@gmail.com>
2021-04-19 15:43:09 +0200

    #45 Implemented +, -, / on config parameter binding

--- libs/auto_disc/utils/spaces/utils/config_parameter_binding.py
+++ libs/auto_disc/utils/spaces/utils/config_parameter_binding.py
@@ -1,4 +1,5 @@
 import json
+import math
 
 class ConfigParameterBinding():
     '''
@@ -6,33 +7,63 @@ class ConfigParameterBinding():
     '''
     def __init__(self, parameter_name):
         self.parameter_name = parameter_name
-        self._other = []
+        self._operations = []
+
+    def _apply_operation(self, val1, val2, operator):
+        if operator == '+':
+            return val1 + val2
+        elif operator == '-':
+            return val1 - val2
+        elif operator == '*':
+            return val1 * val2
+        elif operator == '/':
+            return val1 / val2
 
     def __get__(self, obj, objtype=None):
         value = obj.config[self.parameter_name]
-        for other in self._other:
+        for operation in self._operations:
+            other = operation[0]
+            operator = operation[1]
             if isinstance(other, ConfigParameterBinding):
-                other_value = other.__get__(obj)
-            else:
-                other_value = other
-            value *= other_value
+                other = other.__get__(obj)
+            value = self._apply_operation(value, other, operator)
         
         return value
 
-    def __mul__(self, other):
-        self._other.append(other)
+    def __add__(self, other):
+        self._operations.append((other, '+'))
         return self
 
-    def __rmul__(self, other):
-        self._other.append(other)
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        self._operations.append((other, '-'))
         return self
+
+    def __rsub__(self, other):
+        return self.__sub__(other)
+
+    def __mul__(self, other):
+        self._operations.append((other, '*'))
+        return self
+    
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __div__(self, other):
+        self._operations.append((other, '/'))
+        return self
+
+    def __rdiv__(self, other):
+        return self.__div__(other)
 
     def to_json(self):
-        binding = self.parameter_name
-        for other in self._other:
+        binding = 'binding.' + self.parameter_name
+        for operation in self._operations:
+            other = operation[0]
+            operator = operation[1]
             if isinstance(other, ConfigParameterBinding):
-                binding = binding + '*' + other.parameter_name
-            else:
-                binding = binding + '*' + other
-                
-        return {'binding': binding}
+                other = other.to_json()
+            binding = "({0}, {1}, {2})".format(binding, other, operator)                
+        return binding
