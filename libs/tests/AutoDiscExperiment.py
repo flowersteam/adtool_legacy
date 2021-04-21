90c86331a433a66d485d4e34017ec1fdf89e5ba1
PERIE Mathieu <mathieu.perie@inria.fr>
2021-04-21 22:02:44 +0200

    #47 added lenia output representations hand defined

--- libs/tests/AutoDiscExperiment.py
+++ libs/tests/AutoDiscExperiment.py
@@ -4,7 +4,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(dir_path, "../"))
 
 from auto_disc.systems.python_systems import PythonLenia
-from auto_disc.output_representations.specific import LeniaImageRepresentation
+from auto_disc.output_representations.specific import LeniaImageRepresentation, LeniaHandDefinedRepresentation
 from auto_disc.input_wrappers.generic import TimesNInputWrapper, CppnInputWrapper
 from auto_disc.explorers import IMGEPExplorer
 
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         system=PythonLenia(scale_init_state=1.0),
         explorer=IMGEPExplorer(),
         input_wrappers=[CppnInputWrapper('init_state')], # Starting from the explorer !
-        output_representations=[LeniaImageRepresentation()], # Starting from the system !
+        output_representations=[LeniaHandDefinedRepresentation()], # Starting from the system !
         on_exploration_callbacks=[CustomPrintCallback("Newly explored output !"), CustomSaveCallback("./experiment_results/")]
     )
 
