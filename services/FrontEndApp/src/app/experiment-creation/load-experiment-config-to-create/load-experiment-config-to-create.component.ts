3b5b307b6e36362fbe21b94d676d60001ab86ee5
PERIE Mathieu <mathieu.perie@inria.fr>
2022-02-09 22:35:44 +0100

    #98 create experiment with JSON file and download JSON file using current experiment config

---
+++ services/FrontEndApp/src/app/experiment-creation/load-experiment-config-to-create/load-experiment-config-to-create.component.ts
@@ -0,0 +1,69 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-load-experiment-config-to-create',
+  templateUrl: './load-experiment-config-to-create.component.html',
+  styleUrls: ['./load-experiment-config-to-create.component.scss']
+})
+export class LoadExperimentConfigToCreateComponent implements OnInit {
+
+  @Input() currentExperiment?: any;
+
+  fileName = '';
+  fileContent : string | ArrayBuffer = '';
+  constructor(private http: HttpClient) { }
+  
+  ngOnInit(): void {}
+
+  downloadJson(){
+    var sJson = JSON.stringify(this.currentExperiment);
+    var element = document.createElement('a');
+    element.setAttribute('href', "data:text/json;charset=UTF-8," + encodeURIComponent(sJson));
+    element.setAttribute('download', this.currentExperiment.experiment.name);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click(); // simulate click
+    document.body.removeChild(element);
+  }
+
+  onFileSelected(event:any) {
+
+    const file:File = event.target.files[0];
+
+    const fr = new FileReader();
+    fr.onload = () => {
+      if(fr.result != null){this.fileContent = fr.result};
+      console.log('Commands', this.fileContent);
+      this.setExperimentWithJSON();
+    }
+
+      if (file) {
+
+          this.fileName = file.name;
+
+          const formData = new FormData();
+
+          formData.append("thumbnail", file);
+
+          // const upload$ = this.http.post("/api/thumbnail-upload", formData);
+
+          // upload$.subscribe();
+          fr.readAsText(file);
+          
+      }
+  }
+
+  setExperimentWithJSON(){
+    let loadedExperiment = JSON.parse(this.fileContent.toString());
+    this.currentExperiment.callbacks = loadedExperiment.callbacks;
+    this.currentExperiment.experiment = loadedExperiment.experiment;
+    this.currentExperiment.explorer = loadedExperiment.explorer;
+    this.currentExperiment.input_wrappers = loadedExperiment.input_wrappers;
+    this.currentExperiment.logger_handlers = loadedExperiment.logger_handlers;
+    this.currentExperiment.output_representations = loadedExperiment.output_representations;
+    this.currentExperiment.system = loadedExperiment.system;
+
+  }
+
+}
