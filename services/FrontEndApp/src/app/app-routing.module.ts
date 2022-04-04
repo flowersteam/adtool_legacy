6da5ad8dafab0ccc8f6b242fd9ac9e33ebc98abd
ClementRomac <clement.romac@gmail.com>
2022-04-04 12:32:16 +0200

    #62 Added logo + favicon as well as about page with citation

--- services/FrontEndApp/src/app/app-routing.module.ts
+++ services/FrontEndApp/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ExperimentCreationComponent } from './experiment-creation/experiment-creation.component';
 import { ExperimentMonitoringComponent } from './experiment-monitoring/experiment-monitoring.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'new-experiment', component: ExperimentCreationComponent },
-  { path: 'experiment/:id', component: ExperimentMonitoringComponent }
+  { path: 'experiment/:id', component: ExperimentMonitoringComponent },
+  { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
