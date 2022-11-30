2e89ef88a2c523df95e1e163d516d1413b300a12
Jesse Y Lin <jesseyounglin@gmail.com>
2022-11-30 16:03:03 +0100

    Added debug flag for frontendapp

---
+++ services/FrontEndApp/bash/custom-entrypoint.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+if [[ -z "${DEBUG}" ]]; then
+    DEBUG=0
+else
+    DEBUG=${DEBUG}
+fi
+
+# note: please be in the right workdir before doing this
+if [ $DEBUG -eq 0 ]; then
+    /docker-entrypoint.sh nginx -g "daemon off;"
+else
+    ng serve --configuration development --host 0.0.0.0 --port 80
+fi
