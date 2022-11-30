8330490a8c4cbe6d02ccc449c5d6d9a138e23623
mperie <mathieu.perie@inria.fr>
2022-11-30 13:51:47 +0100

    #133 fixed amd64 appdb api

--- start_app.sh
+++ start_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-chmod 754 -R services/JupyterLab/Notebooks/
-docker-compose -f services/docker-compose.yml --profile prod up --build
+chmod 777 -R services/JupyterLab/Notebooks/
+docker compose -f services/docker-compose.yml --profile prod up --build
 
 # saves docker logs on the disk
 source services/.env
@@ -9,8 +9,8 @@ mkdir -p ${LOG_FILE}
 chmod 754 -R ${LOG_FILE}
 currentTime=$(date '+%F_%H:%M:%S')
 
-for value in app-db app-db-api expe-db expe-db-api autodisc-server jupyter app; do
+for value in app-db app-db-api expe-db expe-db-api autodisc-server jupyter app gateway; do
     mkdir -p ${LOG_FILE}/${value} && touch "${LOG_FILE}/${value}/${currentTime}.log"
-    docker-compose -f services/docker-compose.yml logs ${value} >"${LOG_FILE}/${value}/${currentTime}.log"
-    echo "" >$(docker inspect --format='{{.LogPath}}' services_${value}_1)
+    docker compose -f services/docker-compose.yml logs ${value} >"${LOG_FILE}/${value}/${currentTime}.log"
+    echo "" >$(docker inspect --format='{{.LogPath}}' services-${value}-1)
 done
