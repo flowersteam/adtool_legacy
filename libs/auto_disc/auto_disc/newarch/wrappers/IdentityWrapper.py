bedf57dd1dc0e619928df2b92f72b477038cf38a
LIN Jesse <jesse.lin@inria.fr>
2023-01-10 10:24:12 +0100

    docstring

--- libs/auto_disc/auto_disc/newarch/wrappers/IdentityWrapper.py
+++ libs/auto_disc/auto_disc/newarch/wrappers/IdentityWrapper.py
@@ -1,9 +1,13 @@
 from copy import deepcopy
-from addict import Dict
+from typing import Dict
 from leafutils.leafstructs.stateless import StatelessService
 
 
 class IdentityWrapper(StatelessService):
+    """
+    Wrapper which passes the input without mutation.
+    """
+
     def __init__(self, wrapped_key: str = None) -> None:
         super().__init__()
         # wrapped_key is not used
