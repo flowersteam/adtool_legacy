40aa5e22dadfb24b1a9d8ec5d5cfd1387357a7c6
LIN Jesse <jesse.lin@inria.fr>
2023-02-15 14:35:35 +0100

    init commit of IMGEP

---
+++ libs/auto_disc/auto_disc/newarch/explorers/imgep_explorer.py
@@ -0,0 +1,89 @@
+from leaf.leaf import Leaf
+from leaf.locators import FileLocator
+from auto_disc.newarch.wrappers.IdentityWrapper import IdentityWrapper
+from typing import Dict, Tuple, Callable
+import torch
+from copy import deepcopy
+
+
+class IMGEPExplorer(Leaf):
+    def __init__(self,
+                 premap_key: str = "output",
+                 postmap_key: str = "params",
+                 parameter_map: Leaf = IdentityWrapper(),
+                 behavior_map: Leaf = IdentityWrapper(),
+                 mutator: Leaf = torch.nn.Identity(),
+                 equil_time: int = 0) -> None:
+        super().__init__()
+        self.locator = FileLocator()
+        self.premap_keys = premap_key
+        self.postmap_keys = postmap_key
+        self.parameter_map = parameter_map
+        self.behavior_map = behavior_map
+        self.equil_time = equil_time
+        self.timestep = 0
+
+        self.mutator = mutator
+
+    def map(self, system_output: Dict) -> Dict:
+        """
+        The "map" when the `Explorer` is viewed as a function, which takes the
+        feature vector in behavior space and maps it to a (randomly chosen)
+        subsequent parameter configuration to try.
+        """
+        behavior_tensor, orig_data = self.observe_results(
+            system_output)
+
+        data_shape = behavior_tensor.size()
+
+        params_trial = self.suggest_trial(data_shape)
+
+        new_trial_data = deepcopy(orig_data)
+        del new_trial_data[self.premap_keys]
+        new_trial_data[self.postmap_keys] = params_trial
+
+        self.timestep += 1
+
+        return new_trial_data
+
+    def suggest_trial(self, data_shape: Tuple) -> torch.Tensor:
+        """
+        Samples according to the policy a new trial of parameters for the
+        system, generating sample according to the data_shape
+        """
+        if self.timestep < self.equil_time:
+            params_trial = self.parameter_map.sample(data_shape)
+        else:
+            goal_history = self.behavior_map.get_tensor_history()
+            param_history = self.parameter_map.get_tensor_history()
+
+            goal = self.behavior_map.sample()
+            source_policy_idx = self._find_closest(goal, goal_history)
+            source_policy = param_history[source_policy_idx]
+
+            params_trial = self.mutator(source_policy)
+
+        return params_trial
+
+    def observe_results(self, system_output: Dict) -> Tuple[
+        torch.Tensor, Callable[[], torch.Tensor], Dict
+    ]:
+        """
+        Reads the behavior discovered and processes it.
+        """
+        output_dict = self.behavior_map.map(system_output)
+
+        # for convenience, return a pointer to the feature tensor and
+        behavior_tensor = output_dict[self.premap_keys]
+
+        return behavior_tensor, output_dict
+
+    def optimize():
+        """
+        Optimization step for online learning of the `Explorer` policy.
+        """
+        pass
+
+    def _find_closest(self, goal: torch.Tensor, goal_history: torch.Tensor):
+        # TODO: simple L2 distance right now
+        return torch.argmin((goal_history-goal).pow(2).sum(-1))
---
+++ libs/auto_disc/auto_disc/newarch/explorers/tests/test_imgep_explorer.py
@@ -0,0 +1,145 @@
+from auto_disc.newarch.explorers.imgep_explorer import IMGEPExplorer
+from auto_disc.newarch.maps.MeanBehaviorMap import MeanBehaviorMap
+from auto_disc.newarch.maps.UniformParameterMap import UniformParameterMap
+import torch
+import pathlib
+import os
+import shutil
+
+
+def setup_function(function):
+    global RESOURCE_URI
+    file_path = str(pathlib.Path(__file__).parent.resolve())
+    RESOURCE_URI = os.path.join(file_path, "tmp")
+    os.mkdir(RESOURCE_URI)
+    return
+
+
+def teardown_function(function):
+    global RESOURCE_URI
+    if os.path.exists(RESOURCE_URI):
+        shutil.rmtree(RESOURCE_URI)
+    return
+
+
+def test___init__():
+    mean_map = MeanBehaviorMap(premap_key="output")
+    param_map = UniformParameterMap(premap_key="params",
+                                    tensor_low=torch.tensor([0., 0., 0.]),
+                                    tensor_high=torch.tensor([2., 2., 2.]))
+    explorer = IMGEPExplorer(premap_key="output", postmap_key="params",
+                             parameter_map=param_map, behavior_map=mean_map,
+                             equil_time=2)
+    assert explorer.behavior_map == mean_map
+    assert explorer.parameter_map == param_map
+    assert explorer.equil_time == 2
+    assert explorer.timestep == 0
+
+
+def test__find_closest():
+    goal_history = torch.tensor(
+        [[1, 2, 3], [4, 5, 6], [-1, -1, -1]], dtype=float)
+    goal = torch.tensor([-0.99, -0.99, -0.99])
+
+    mean_map = MeanBehaviorMap(premap_key="output")
+    param_map = UniformParameterMap(premap_key="params",
+                                    tensor_low=torch.tensor([0., 0., 0.]),
+                                    tensor_high=torch.tensor([2., 2., 2.]))
+    explorer = IMGEPExplorer(premap_key="output", postmap_key="params",
+                             parameter_map=param_map, behavior_map=mean_map,
+                             equil_time=2)
+
+    idx = explorer._find_closest(goal, goal_history)
+    assert idx == torch.tensor([2])
+    assert torch.allclose(goal_history[idx], torch.tensor([-1., -1., -1.]))
+
+
+def test_observe_results():
+    mean_map = MeanBehaviorMap(premap_key="output")
+    param_map = UniformParameterMap(premap_key="params",
+                                    tensor_low=torch.tensor([0., 0., 0.]),
+                                    tensor_high=torch.tensor([2., 2., 2.]))
+    explorer = IMGEPExplorer(premap_key="output", postmap_key="params",
+                             parameter_map=param_map, behavior_map=mean_map,
+                             equil_time=2)
+
+    system_output = {"metadata": 1, "output": torch.tensor([1., 1., 1.])}
+    behavior, output_dict = explorer.observe_results(system_output)
+
+    assert behavior == output_dict["output"]
+    behavior += 1
+    assert torch.allclose(output_dict["output"], torch.tensor([2., 2., 2.]))
+
+
+def test_map():
+    mean_map = MeanBehaviorMap(premap_key="output")
+    param_map = UniformParameterMap(premap_key="params",
+                                    tensor_low=torch.tensor([0., 0., 0.]),
+                                    tensor_high=torch.tensor([6., 6., 6.]))
+    explorer = IMGEPExplorer(premap_key="output", postmap_key="params",
+                             parameter_map=param_map, behavior_map=mean_map,
+                             equil_time=2)
+    system_output = {"metadata": 1, "output": torch.tensor([1., 2., 3.])}
+
+    new_params = explorer.map(system_output)
+    assert new_params["params"]
+    assert new_params.get("output", None) is None
+    system_output["metadata"] = 2
+    assert new_params["metadata"] == 1
+    assert explorer.timestep == 1
+
+
+def test_suggest_trial_equilibriation():
+    mean_map = MeanBehaviorMap(premap_key="output")
+    param_map = UniformParameterMap(premap_key="params",
+                                    tensor_low=torch.tensor([0., 0., 0.]),
+                                    tensor_high=torch.tensor([2., 2., 2.]))
+    explorer = IMGEPExplorer(premap_key="output", postmap_key="params",
+                             parameter_map=param_map, behavior_map=mean_map,
+                             equil_time=2)
+
+    for _ in range(2):
+        params_trial = explorer.suggest_trial(torch.Size([3]))
+        assert params_trial.size() == torch.Size([3])
+
+
+def test_suggest_trial_behavioral_diffusion():
+    mean_map = MeanBehaviorMap(premap_key="output")
+    param_map = UniformParameterMap(premap_key="params",
+                                    tensor_low=torch.tensor([0., 0., 0.]),
+                                    tensor_high=torch.tensor([6., 6., 6.]))
+    explorer = IMGEPExplorer(premap_key="output", postmap_key="params",
+                             parameter_map=param_map, behavior_map=mean_map,
+                             equil_time=2)
+
+    # mock history
+    mock_system_output_history = \
+        [{"metadata": 1,
+          "output": torch.tensor([1., 2., 3.])},
+         {"metadata": 1,
+          "output": torch.tensor([4., 5., 6.])},
+         {"metadata": 1,
+          "output": torch.tensor([0., 0., 0.])}]
+    mock_param_input_history = \
+        [{"metadata": 1,
+          "params": torch.tensor([0., 1., 2.])},
+         {"metadata": 1,
+          "params": torch.tensor([3., 4., 5.])},
+         {"metadata": 1,
+          "params": torch.tensor([-1., -1., -1.])}]
+    explorer.behavior_map.history_saver.buffer = \
+        [{"metadata": 1,
+          "output": torch.tensor([2.])},
+         {"metadata": 1,
+          "output": torch.tensor([5.])},
+         {"metadata": 1,
+          "output": torch.tensor([0.])}]
+    explorer.parameter_map.history_saver.buffer = mock_param_input_history
+    explorer.behavior_map.projector.low = torch.tensor([0.])
+    explorer.behavior_map.projector.high = torch.tensor([5.])
+    explorer.behavior_map.projector.tensor_shape = torch.Size([1])
+    explorer.timestep = 2
+
+    # actual test
+    params_trial = explorer.suggest_trial(torch.Size([3]))
+    assert params_trial.size() == torch.Size([3])
