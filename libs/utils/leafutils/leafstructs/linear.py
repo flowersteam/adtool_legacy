d2570f0b52dbcd4737709b5d011ad1522a751aa2
LIN Jesse <jesse.lin@inria.fr>
2023-03-01 16:54:27 +0100

    reload parent_id for rewinding commits

--- libs/utils/leafutils/leafstructs/linear.py
+++ libs/utils/leafutils/leafstructs/linear.py
@@ -87,6 +87,8 @@ class LinearLocator(Locator):
         """
         try:
             db_name, row_id = uid.split(":")
+            # set parent_id from retrieval
+            self.parent_id = int(row_id)
         # check in case too many strings are returned
         except ValueError:
             raise ValueError("leaf_uid is not properly formatted.")
