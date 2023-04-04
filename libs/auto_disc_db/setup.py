863673b56274dbe0c12f5587123073798b219dac
LIN Jesse <jesse.lin@inria.fr>
2023-04-04 16:12:52 +0200

    commented as json comes by default in python

--- libs/auto_disc_db/setup.py
+++ libs/auto_disc_db/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import sys
 
-assert sys.version_info.major == 3 and sys.version_info.minor in [6, 7] , \
+assert sys.version_info.major == 3 and sys.version_info.minor in [6, 7], \
     "This repo is designed to work with Python 3.6 or 3.7." \
     + "Please install it before proceeding."
 
@@ -11,7 +11,7 @@ setup(
     version="1.0",
     install_requires=[
         'requests==2.28.1',
-        'json'
+        # 'json'
     ],
     description="auto_disc_db Python lib",
     author="Flowers Team Inria",
