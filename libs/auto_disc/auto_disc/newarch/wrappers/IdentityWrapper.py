2db4e561a1aeccf6c7e2b8c7f73f2df8fb4a5cff
LIN Jesse <jesse.lin@inria.fr>
2023-04-05 15:48:28 +0200

    removing Stateless locator assumptions from cls

--- libs/auto_disc/auto_disc/newarch/wrappers/IdentityWrapper.py
+++ libs/auto_disc/auto_disc/newarch/wrappers/IdentityWrapper.py
@@ -1,5 +1,6 @@
 from auto_disc.newarch.wrappers.TransformWrapper import TransformWrapper
 from leaf.Leaf import Leaf
+from leaf.locators.locators import BlobLocator
 from typing import Dict, List
 
 
@@ -10,6 +11,9 @@ class IdentityWrapper(TransformWrapper):
 
     def __init__(self, premap_keys: List[str] = []) -> None:
         super().__init__()
+        # here only for explicitness
+        # as the parent class already does it
+        self.locator = BlobLocator()
 
     def map(self, input: Dict) -> Dict:
         return super().map(input)
