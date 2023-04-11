c857e81d0a19e7a9c49ed8abbe599fa3dcce984a
LIN Jesse <jesse.lin@inria.fr>
2023-04-11 18:36:15 +0200

    added mutator for custom objects

--- libs/auto_disc/auto_disc/newarch/wrappers/mutators.py
+++ libs/auto_disc/auto_disc/newarch/wrappers/mutators.py
@@ -1,4 +1,5 @@
 import torch
+from typing import Any
 
 
 def add_gaussian_noise(input_tensor: torch.Tensor,
@@ -12,3 +13,19 @@ def add_gaussian_noise(input_tensor: torch.Tensor,
     noise_unit = torch.randn(input_tensor.size())
     noise = noise_unit*std + mean
     return input_tensor + noise
+
+
+def call_mutate_method(input_object: Any):
+    """
+    If parameters are given as a custom object, then the object must have a 
+    `mutate` method in order to mutate the underlying parameters controlling
+    the object.
+    """
+    # quick test to check if `mutate` method returns a new object or
+    # modifies in place the input_object
+    # if it does something else, this is a user error
+    capture_out = input_object.mutate()
+    if capture_out is not None:
+        return capture_out
+    else:
+        return input_object
