c094c1b96d6bba99d3edadacdb34f6e7049d6661
LIN Jesse <jesse.lin@inria.fr>
2023-04-12 17:40:58 +0200

    added LeniaCPPN

---
+++ libs/auto_disc/auto_disc/newarch/systems/tests/test_LeniaCPPN.py
@@ -0,0 +1,50 @@
+from auto_disc.newarch.systems.LeniaCPPN import LeniaCPPN
+from auto_disc.newarch.maps.NEATParameterMap import NEATParameterMap
+import torch
+from copy import deepcopy
+import os
+
+
+def setup_function(function):
+    global dummy_input
+
+    tests_path = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(tests_path, "config_test.cfg")
+
+    dummy_input = {}
+    neat_map = NEATParameterMap(config_path=config_path)
+    genome_dict = neat_map.map(dummy_input)
+
+    dummy_input = \
+        {
+            "params": {
+                "dynamic_params":
+                {
+                    "R": torch.tensor(5.),
+                    "T": torch.tensor(10.),
+                    "b": torch.tensor([0.1, 0.2, 0.3, 0.4]),
+                    "m": torch.tensor(0.5),
+                    "s": torch.tensor(0.1)
+                },
+                "genome": genome_dict["genome"],
+                "neat_config": genome_dict["neat_config"]
+            }
+        }
+
+
+def teardown_function(function):
+    pass
+
+
+def test___init__():
+    system = LeniaCPPN()
+
+
+def test_map():
+    system = LeniaCPPN()
+    out = system.map(dummy_input)
+
+    assert "output" in out
+    assert out["output"].size() == (256, 256)
+    assert system.lenia.orbit[-1].size() == (256, 256)
+    assert out["output"] is not system.lenia.orbit[-1]
