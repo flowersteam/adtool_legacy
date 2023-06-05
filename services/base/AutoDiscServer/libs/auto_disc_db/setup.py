c38250f8b327fa9b50d39d735e918a7cd4b215fe
LIN Jesse <jesse.lin@inria.fr>
2023-06-05 16:28:05 +0200

    fix(auto_disc_db): remove version restriction

--- services/base/AutoDiscServer/libs/auto_disc_db/setup.py
+++ services/base/AutoDiscServer/libs/auto_disc_db/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 import sys
 
-assert sys.version_info.major == 3 and sys.version_info.minor in [6, 7], \
-    "This repo is designed to work with Python 3.6 or 3.7." \
-    + "Please install it before proceeding."
+# assert sys.version_info.major == 3 and sys.version_info.minor in [6, 7], \
+#     "This repo is designed to work with Python 3.6 or 3.7." \
+#     + "Please install it before proceeding."
 
 setup(
     name='auto_disc_db',
