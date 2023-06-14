dbfb0d4d2bfe204b1bd86c905e951b368e210c0b
Jesse Y Lin <jesseyounglin@gmail.com>
2023-06-14 18:54:18 +0200

    docs: add callback docstr

---
+++ services/base/AutoDiscServer/libs/auto_disc/auto_disc/utils/callbacks/__init__.py
@@ -0,0 +1,4 @@
+"""At certain defined points in the exploration loop, the software executes
+callbacks which may be user defined. Such callbacks are mostly used for doing
+environment setup and teardown, for saving output, or for accepting user input.
+"""
