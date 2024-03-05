37ff9110500ce91e71ed3bdf3035928d27555434
wd400 <zacharie.bugaud@inria.fr>
2024-03-05 14:39:42 +0100

    Update environment variables for debugging

--- services/base/FrontEndApp/angular_app/src/assets/env.js
+++ services/base/FrontEndApp/angular_app/src/assets/env.js
@@ -4,5 +4,5 @@
   // Environment variables
   window["env"]["GATEWAY_PORT"] = "4201";
   window["env"]["GATEWAY_HOST"] = "127.0.0.1";
-  window["env"]["debug"] = "";
+  window["env"]["debug"] = "4201";
 })(this);
